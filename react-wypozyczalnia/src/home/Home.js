c99212958270670fcb3d1e7527cd5de7bb2bb8a3
Swiftlover13 <fokt.paula@gmail.com>
2024-06-28 09:52:32 +0200

    zmiany

--- react-wypozyczalnia/src/home/Home.js
+++ react-wypozyczalnia/src/home/Home.js
@@ -1,11 +1,13 @@
 import ItemList from "../components/item/ItemList";
 import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <ItemList />
+      <Footer />
     </div>
   );
 };
